27f5b68449e3e2ce1459c6ce52dc65b4c91886e9
Solaja Emmanuel <emmanueladebola98@gmail.com>
2025-02-27 17:42:03 +0100

    link fix

--- src/pages/project/Project.jsx
+++ src/pages/project/Project.jsx
@@ -65,7 +65,7 @@ export const Project = () => {
                     <img className={Styles.projectPic} src={ "/images/coin.png" } alt=""/>
                     <p className={Styles.projectDescription}>
                       <div className={Styles.p1}>
-                        Crypto Tracker
+                        Coin Tracker
                       </div>
                       <div className={Styles.p2}>
                         This is a Crypto tracker website with an Api implementation.
@@ -74,12 +74,12 @@ export const Project = () => {
                         React
                       </div>
                       <div className={Styles.p4}>
-                        <a href="" target="_blank" className={Styles.projectLink}>
+                        <a href="https://github.com/Debola23/Coins" target="_blank" className={Styles.projectLink}>
                           <svg xmlns="http://www.w3.org/2000/svg" width="22" height="24" fill="currentColor" viewBox="0 0 16 16">
                             <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"></path>
                           </svg>
                         </a>
-                        <a href="" target="_blank" className={Styles.projectLink}>
+                        <a href="https://coins-red.vercel.app/" target="_blank" className={Styles.projectLink}>
                           <svg xmlns="http://www.w3.org/2000/svg" width="22" height="24" fill="currentColor" viewBox="0 0 16 16">
                             <path d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"></path>
                             <path  d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"></path>
